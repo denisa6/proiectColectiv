a33bdd7806f8f946e069b2a84a2890aeefcff41e
914-Claudia-Moisiuc <91720149+914-Claudia-Moisiuc@users.noreply.github.com>
2023-11-10 12:36:18 +0200

    br1 - added for now views and serialiser for user

--- user/views.py
+++ user/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+
+from user.models import RegularUser
+from user.serialisers import RegularUserSerializer
+
 
 # Create your views here.
+# TODO a better view for the user
+
+class RegularUserListView(viewsets.ModelViewSet):
+    queryset = RegularUser.objects.all()
+    serializer_class = RegularUserSerializer
