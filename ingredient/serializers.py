50c58e502c173cacfa7af282e05857294a5c3607
914DianaJugariu <dia.jugariu4@gmail.com>
2023-11-15 05:22:56 -0800

    Change price in ingredient.

--- ingredient/serializers.py
+++ ingredient/serializers.py
@@ -4,8 +4,6 @@ from ingredient.models import Ingredient
 
 
 class IngredientSerializers(serializers.ModelSerializer):
-    price = serializers.CharField(source='get_price_display')
-
     class Meta:
         model = Ingredient
         fields = ['id', 'name', 'price', 'calorie', 'tags', 'allergens']
