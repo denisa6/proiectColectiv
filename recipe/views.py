17aa3200dacf91e93ca683108f1d80c152cb30e6
914-Claudia-Moisiuc <91720149+914-Claudia-Moisiuc@users.noreply.github.com>
2023-12-28 17:38:05 +0200

    added validation error for login added filters for recipe

--- recipe/views.py
+++ recipe/views.py
@@ -10,3 +10,36 @@ from recipe.serializers import RecipeSerializer
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
+    def get_queryset(self):
+        recipes = Recipe.objects.all()
+        request_data = self.request.GET
+        if 'difficulty' in request_data:
+            recipes = recipes.filter(difficulty=int(request_data['difficulty']))
+        if 'name' in request_data:
+            recipes = recipes.filter(name__icontains=request_data['name'])
+        if 'ingredients' in request_data:
+            recipes = Recipe.objects.filter(ingredients__id=int(request_data['ingredients']))
+        if 'time' in request_data:
+            recipes = Recipe.objects.filter(time_min__lte=int(request_data['time']), time_max__gte=int(request_data['time']))
+        if 'number_people' in request_data:
+            recipes = Recipe.objects.filter(number_people=request_data['number_people'])
+        if 'type_recipe' in request_data:
+            recipes = Recipe.objects.filter(type_recipe=request_data['type_recipe'])
+        if 'estimated_price_min' in request_data:
+            if 'estimated_price_max' in request_data:
+                recipes = Recipe.objects.filter(estimated_price__gte=int(request_data['estimated_price_min']),
+                                                estimated_price__lte=int(request_data['estimated_price_max']))
+            else:
+                recipes = Recipe.objects.filter(estimated_price__gte=int(request_data['estimated_price_min']))
+        elif 'estimated_price_max' in request_data:
+            recipes = Recipe.objects.filter(estimated_price__lte=int(request_data['estimated_price_max']))
+        if 'total_calories_min' in request_data:
+            if 'total_calories_max' in request_data:
+                recipes = Recipe.objects.filter(total_calories__gte=int(request_data['total_calories_min']),
+                                                total_calories__lte=int(request_data['total_calories_max']))
+            else:
+                recipes = Recipe.objects.filter(total_calories__gte=int(request_data['total_calories_min']))
+        elif 'total_calories_max' in request_data:
+            recipes = Recipe.objects.filter(total_calories__lte=int(request_data['total_calories_max']))
+        return recipes
